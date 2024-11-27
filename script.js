90ac9195f71af4011a8f8debdbc87ea08858dcce
fldup <lilas.grenier@etu.univ-lyon1.fr>
2024-11-27 15:25:36 +0100

    active

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 function addItem() {
-    var item = document.getElementById("new-todo-item-title").value;
+    const item = document.getElementById("new-todo-item-title").value;
     document.getElementById("todo-list").innerHTML += "<li>" + item;
     document.getElementById("todo-list").innerHTML += "<button onclick='deleteItem()'>Delete</button>";
-    document.getElementById("todo-list").innerHTML += "<button onclick='editItem()'>Edit</button> </li>";
+    document.getElementById("todo-list").innerHTML += "<button onclick='editItem(" + item + ")'>Edit</button> </li>";
     document.getElementById("new-todo-item-title").value = "";
 }
 
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+
 //pas encore fonctionnel
 function editItem() {
-    var item = document.getElementById("edit-todo-item-title").value;
+    let x = document.getElementById("next-item");
+    let y = document.getElementById("edit-item");
+    if (x.style.display == "hidden") {
+        x.style.display = "block";
+        y.style.display = "hidden";
+      } else {
+        x.style.display = "hidden";
+        y.style.display = "block";
+      }
+    const item = document.getElementById("edit-todo-item-title").value;
     document.getElementById("todo-list").innerHTML += "<li>" + item + "</li>";
+
     document.getElementById("edit-todo-item-title").value = "";
 }
 
@@ -17,6 +39,6 @@ function editItem() {
 function cancelEditItem() {}
 
 //pas encore fonctionnel
-function deleteItem() {
-    document.getElementById("element").innerHTML = "";
+function deleteItem(item) {
+    item = "";
 }
